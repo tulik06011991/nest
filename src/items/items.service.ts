f4f125fd4d54edee35265ff91bf6a8d48f8c3dda
tolqin <tolqinmirsaliyev@gmail.com>
2024-11-21 11:04:11 +0500

    vcxvdsgfdsz

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -9,27 +9,28 @@ import { Items } from './entities/item.entity';
 @Injectable()
 export class ItemsService {
 
-constructor(@InjectModel(Items.name) private ItemsModel: Model<Items> ){}
+  constructor(@InjectModel(Items.name) private ItemsModeil : Model<Items>){}
+
 
   create(createItemDto: CreateItemDto): Promise<Items> {
-    const results =   new this.ItemsModel(createItemDto);
+    const results = new this.ItemsModeil(createItemDto)
     return results.save()
-
   }
 
   findAll(): Promise<Items[]> {
-    return  this.ItemsModel.find().exec();
+    return this.ItemsModeil.find().exec()
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} item`;
+  findOne(id: number): Promise<Items> {
+    return  this.ItemsModeil.findById(id).exec()
   }
 
-  update(id: number, updateItemDto: UpdateItemDto) {
-    return `This action updates a #${id} item`;
+  update(id: number, updateItemDto: UpdateItemDto): Promise<Items> {
+    return this.ItemsModeil.findByIdAndUpdate(id, updateItemDto, { new: true }).exec();
   }
+  
 
-  remove(id: number) {
-    return `This action removes a #${id} item`;
+  remove(id: number):Promise<Items> {
+    return this.ItemsModeil.findByIdAndDelete(id).exec()
   }
 }
